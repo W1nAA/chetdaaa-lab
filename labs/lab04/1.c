c8f36a951a61aca45a848c64f45b2acd43d7b533
monarrrrr <monarita.xx@gmail.com>
2026-09-19 16:14:44 +0700

    Implement heap sort

--- labs/lab04/1.c
+++ labs/lab04/1.c
@@ -1,5 +1,80 @@
 #include <stdio.h>
 
-int main(){
-    printf("Heap Sort & Distribution Sort Task 1");
+typedef struct {
+    char portfolioId[10];
+    double riskScore;
+} Portfolio;
+
+void swapPortfolio(Portfolio *a, Portfolio *b)
+{
+    Portfolio temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(Portfolio portfolios[], int n, int i)
+{
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n &&
+        portfolios[left].riskScore < portfolios[smallest].riskScore)
+    {
+        smallest = left;
+    }
+
+    if (right < n &&
+        portfolios[right].riskScore < portfolios[smallest].riskScore)
+    {
+        smallest = right;
+    }
+
+    if (smallest != i)
+    {
+        swapPortfolio(&portfolios[i], &portfolios[smallest]);
+        heapify(portfolios, n, smallest);
+    }
+}
+
+void heapSort(Portfolio portfolios[], int n)
+{
+    int i;
+
+    for (i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(portfolios, n, i);
+    }
+
+    for (i = n - 1; i > 0; i--)
+    {
+        swapPortfolio(&portfolios[0], &portfolios[i]);
+        heapify(portfolios, i, 0);
+    }
+}
+
+int main()
+{
+    Portfolio portfolios[] = {
+        {"PF01", 45.2},
+        {"PF02", 91.8},
+        {"PF03", 12.5},
+        {"PF04", 78.3}
+    };
+
+    int n = sizeof(portfolios) / sizeof(portfolios[0]);
+    int i;
+
+    heapSort(portfolios, n);
+
+    printf("Heap Sort:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%s %.1f\n",
+               portfolios[i].portfolioId,
+               portfolios[i].riskScore);
+    }
+
+    return 0;
 }
